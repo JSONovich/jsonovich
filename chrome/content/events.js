8b1d0d1731a81a0e9754a9e70bfb915094082acd
michael.giarlo <michael.giarlo@4a0d538c-d5f5-11dd-a297-4fb579711a53>
2009-11-06 21:44:09 +0000

    Fixes unicode input bug and adds namespacing to the overlay

--- chrome/content/events.js
+++ chrome/content/events.js
@@ -1,62 +1,65 @@
-const EXTENSION_ID = "jsonovich@lackoftalent.org";
-
-var UninstallObserver = {
-  _uninstall : false,
-
-  observe: function (aSubject, aTopic, aData) {
-    if (aTopic == "em-action-requested") {
-      aSubject.QueryInterface(Ci.nsIUpdateItem);
-      if (aSubject.id == EXTENSION_ID) {
-	if (aData == "item-uninstalled") {
-	  this._uninstall = true;
-	}
-	else if (data == "item-cancel-action") {
-	  this._uninstall = false;
-	}
-      }
-    }
-    else if (aTopic == "quit-application-granted") {
-      if (this._uninstall) {
-	var prefManager = Cc["@mozilla.org/preferences-service;1"]
-			    .getService(Ci.nsIPrefBranch);
-	var acceptHeader = prefManager.getCharPref("network.http.accept.default");
-	if (acceptHeader.indexOf("application/json") != -1) {
-	  acceptHeader = acceptHeader.replace(/,application\/json/, "");
-	  prefManager.setCharPref("network.http.accept.default", acceptHeader);
-	}
-	prefManager.setBoolPref("extensions.jsonovich.installed", false);
-      }
-      this.unregister();
-    }
-  },
-
-  register: function () {
-    var observerService = Cc["@mozilla.org/observer-service;1"]
-			    .getService(Ci.nsIObserverService);
-    observerService.addObserver(this, "em-action-requested", false);
-    observerService.addObserver(this, "quit-application-granted", false);
-  },
-
-  unregister: function () {
-    var observerService = Cc["@mozilla.org/observer-service;1"]
-			    .getService(Ci.nsIObserverService);
-    observerService.removeObserver(this,"em-action-requested");
-    observerService.removeObserver(this,"quit-application-granted");
-  }
+if (!jsonovich) {
+  var jsonovich = {};
 }
 
-var jsonovich_init = function () {
-  var prefManager = Cc["@mozilla.org/preferences-service;1"]
-		      .getService(Ci.nsIPrefBranch);
-  var acceptHeader = prefManager.getCharPref("network.http.accept.default");
-  if (acceptHeader.indexOf("application/json") == -1) {
-    acceptHeader += ",application/json";
-    prefManager.setCharPref("network.http.accept.default", acceptHeader);
+jsonovich.overlay = {
+  init: function () {
+    var prefManager = Cc["@mozilla.org/preferences-service;1"]
+			.getService(Ci.nsIPrefBranch);
+    var acceptHeader = prefManager.getCharPref("network.http.accept.default");
+    if (acceptHeader.indexOf("application/json") == -1) {
+      acceptHeader += ",application/json";
+      prefManager.setCharPref("network.http.accept.default", acceptHeader);
+    }
+    prefManager.setBoolPref("extensions.jsonovich.installed", true);
+    jsonovich.overlay.UninstallObserver.register();
+  },
+
+  UninstallObserver: {
+    _uninstall : false,
+
+    observe: function (aSubject, aTopic, aData) {
+      if (aTopic == "em-action-requested") {
+	aSubject.QueryInterface(Ci.nsIUpdateItem);
+	if (aSubject.id == "jsonovich@lackoftalent.org") {
+	  if (aData == "item-uninstalled") {
+	    jsonovich.overlay.UninstallObserver._uninstall = true;
+	  }
+	  else if (data == "item-cancel-action") {
+	    jsonovich.overlay.UninstallObserver._uninstall = false;
+	  }
+	}
+      }
+      else if (aTopic == "quit-application-granted") {
+	if (jsonovich.overlay.UninstallObserver._uninstall) {
+	  var prefManager = Cc["@mozilla.org/preferences-service;1"]
+			      .getService(Ci.nsIPrefBranch);
+	  var acceptHeader = prefManager.getCharPref("network.http.accept.default");
+	  if (acceptHeader.indexOf("application/json") != -1) {
+	    acceptHeader = acceptHeader.replace(/,application\/json/, "");
+	    prefManager.setCharPref("network.http.accept.default", acceptHeader);
+	  }
+	  prefManager.setBoolPref("extensions.jsonovich.installed", false);
+	}
+	jsonovich.overlay.UninstallObserver.unregister();
+      }
+    },
+
+    register: function () {
+      var observerService = Cc["@mozilla.org/observer-service;1"]
+			      .getService(Ci.nsIObserverService);
+      observerService.addObserver(jsonovich.overlay.UninstallObserver, "em-action-requested", false);
+      observerService.addObserver(jsonovich.overlay.UninstallObserver, "quit-application-granted", false);
+    },
+
+    unregister: function () {
+      var observerService = Cc["@mozilla.org/observer-service;1"]
+			      .getService(Ci.nsIObserverService);
+      observerService.removeObserver(jsonovich.overlay.UninstallObserver,"em-action-requested");
+      observerService.removeObserver(jsonovich.overlay.UninstallObserver,"quit-application-granted");
+    }
   }
-  prefManager.setBoolPref("extensions.jsonovich.installed", true);
+};
 
-  UninstallObserver.register();
-}
-
-window.addEventListener("load", jsonovich_init, false);
+window.addEventListener("load", jsonovich.overlay.init, false);
 
