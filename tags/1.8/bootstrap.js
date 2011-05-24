df56830da2ffbf81a3ed5808beb4aa39f60cc2b2
W.Elwood08@gmail.com <W.Elwood08@gmail.com@4a0d538c-d5f5-11dd-a297-4fb579711a53>
2011-05-24 20:42:55 +0000

    Tag 1.8
    Gecko 2 restartless support

---
+++ tags/1.8/bootstrap.js
@@ -0,0 +1,90 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is JSONovich restartless bootstrap.
+ *
+ * The Initial Developer of the Original Code is
+ * William Elwood <we9@kent.ac.uk>.
+ * Portions created by the Initial Developer are Copyright (C) 2011
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK *****
+ *
+ * This file contains the JSONovich bootstrap for Gecko 2+ browsers.
+ *
+ * Changelog:
+ * [2011-05] - Created FF4 restartless bootstrap for JSONovich extension
+ */
+
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+const Cm = Components.manager;
+const Cr = Components.results;
+const Cu = Components.utils;
+Cu.import("resource://gre/modules/Services.jsm");
+Cu.import("resource://gre/modules/AddonManager.jsm");
+
+let global = this;
+let bootstrapData = {
+    name: 'JSONovich',
+    lname: 'jsonovich',
+    debug: false
+}
+
+function startup(data, reason) {
+    AddonManager.getAddonByID(data.id, function(addon) {
+        /* don't use Cu.import for anything we want to be reloadable without restart
+         * (saves messing with the ugly workaround of changing directories and URLs...) */
+        Services.scriptloader.loadSubScript(addon.getResourceURI("modules/unload.js").spec, global);
+
+        JSONovichBootstrap.setResourceAlias(bootstrapData.lname + '-modules', addon.getResourceURI('modules'));
+        JSONovichBootstrap.setResourceAlias(bootstrapData.lname, addon.getResourceURI('chrome/public'));
+
+        Services.scriptloader.loadSubScript('resource://' + bootstrapData.lname + '-modules/jsonStreamConverter.js', global);
+    });
+}
+
+function shutdown(data, reason) {
+    if(reason != APP_SHUTDOWN) unload();
+    if(reason == ADDON_DISABLE && bootstrapData.debug) {
+        AddonManager.getAddonByID(data.id, function(addon) {
+            addon.userDisabled = false;
+        });
+    }
+}
+function install(data, reason) {}
+function uninstall(data, reason) {}
+
+let JSONovichBootstrap = {
+    setResourceAlias: function(alias, target) {
+        let proto = Services.io.getProtocolHandler("resource").QueryInterface(Ci.nsIResProtocolHandler);
+        proto.setSubstitution(alias, target);
+        unload(function() {
+            proto.setSubstitution(alias, null);
+        });
+    }
+};
