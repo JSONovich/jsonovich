3a164022213321da12d85b29e1b1d17be6276af8
W.Elwood08@gmail.com <W.Elwood08@gmail.com@4a0d538c-d5f5-11dd-a297-4fb579711a53>
2011-05-28 23:33:50 +0000

    License boilerplate was occupying about 1 in 4 non-empty lines overall in JavaScript files, move to license.txt where possible to reduce download size.

--- src/modules/gecko/helper.js
+++ src/modules/gecko/helper.js
@@ -1,39 +1,4 @@
-/* ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1/GPL 2.0/LGPL 2.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is JSONovich Gecko helper module.
- *
- * The Initial Developer of the Original Code is
- * William Elwood <we9@kent.ac.uk>.
- * Portions created by the Initial Developer are Copyright (C) 2011
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- *
- * Alternatively, the contents of this file may be used under the terms of
- * either the GNU General Public License Version 2 or later (the "GPL"), or
- * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the MPL, the GPL or the LGPL.
- *
- * ***** END LICENSE BLOCK *****
- *
+/**
  * This file contains utility functions and initialisation specific to Gecko environments.
  *
  * Changelog:
@@ -44,6 +9,10 @@
  *   bootstrap script (importing or emulating Services.jsm)
  * - Assumes the ADDON_LNAME constant is defined by the relevant bootstrap script
  * - Assumes the getResourceURI function is defined by the relevant bootstrap script
+ *
+ * @license MPL 1.1/GPL 2.0/LGPL 2.1, see license.txt
+ * @author William Elwood <we9@kent.ac.uk>
+ * @copyright 2011 All Rights Reserved
  */
 
 'use strict';
--- src/modules/gecko/prefs.js
+++ src/modules/gecko/prefs.js
@@ -1,59 +1,27 @@
-/* ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1/GPL 2.0/LGPL 2.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is JSONovich Gecko preferences module.
- *
- * The Initial Developer of the Original Code is
- * William Elwood <we9@kent.ac.uk>.
- * Portions created by the Initial Developer are Copyright (C) 2011
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- *  - Gecko 2 emulation from mozilla-central.
- *    Portions Copyright (C) 2010 the Mozilla Foundation.
- *
- * Alternatively, the contents of this file may be used under the terms of
- * either the GNU General Public License Version 2 or later (the "GPL"), or
- * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the MPL, the GPL or the LGPL.
- *
- * ***** END LICENSE BLOCK *****
- *
+/**
  * This file contains utilities for managing addon preferences specific to Gecko environments.
  *
  * Changelog:
  * [2011-05] - Created preferences manager
  *
  * TODO: when dropping Gecko 1.9.2/Firefox 3.6 support, remove emulation of Services.jsm.
+ *
+ * @license MPL 1.1/GPL 2.0/LGPL 2.1, see license.txt
+ * @author William Elwood <we9@kent.ac.uk>
+ * @copyright 2011 All Rights Reserved
+ * Portions Copyright (C) 2010 the Mozilla Foundation.
  */
 
 'use strict';
 
 if(!Services.prefs) { // emulate Services.jsm (introduced in Gecko 2/FF4)
-    // see http://hg.mozilla.org/mozilla-central/diff/78e5543c0bc4/toolkit/content/Services.jsm
+    // @see http://hg.mozilla.org/mozilla-central/diff/78e5543c0bc4/toolkit/content/Services.jsm
     XPCOMUtils.defineLazyGetter(Services, "prefs", function () {
         return Cc["@mozilla.org/preferences-service;1"].getService(Ci.nsIPrefService).QueryInterface(Ci.nsIPrefBranch2);
     });
 }
 if(!Services.contentPrefs) {
-    // see http://hg.mozilla.org/mozilla-central/diff/3ccadf603301/toolkit/content/Services.jsm
+    // @see http://hg.mozilla.org/mozilla-central/diff/3ccadf603301/toolkit/content/Services.jsm
     XPCOMUtils.defineLazyServiceGetter(Services, "contentPrefs", "@mozilla.org/content-pref/service;1", "nsIContentPrefService");
 }
 const NS_PREFBRANCH_PREFCHANGE_TOPIC_ID = "nsPref:changed";
