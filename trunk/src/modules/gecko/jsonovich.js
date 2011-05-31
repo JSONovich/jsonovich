8c9e39724f8292080d424b0c7e272f15ae5da4fd
W.Elwood08@gmail.com <W.Elwood08@gmail.com@4a0d538c-d5f5-11dd-a297-4fb579711a53>
2011-05-31 02:54:13 +0000

    Debug preference listener is now deferred from main startup on a timer because it's non-essential, added more startup instrumentation.

--- trunk/src/modules/gecko/jsonovich.js
+++ trunk/src/modules/gecko/jsonovich.js
@@ -54,39 +54,28 @@ function require(path) {
 
 function startup() {
     let prefs = require(PLATFORM + '/prefs').branch;
+
+    TS['SetDefaultPrefs'] = [Date.now()];
     require('defaultPrefs').setDefaults(prefs('extensions.' + ADDON_LNAME, true).set);
+    TS['SetDefaultPrefs'].push(Date.now());
 
     let listenPref = prefs('extensions.' + ADDON_LNAME).listen;
-    listenPref('debug', function(branch, pref) {
-        let debug = branch.get(pref, 'boolean');
-        let log = require(PLATFORM + '/log');
-        log.setDebug(debug);
-        if(debug) {
-            let desc = {
-                'Bootstrap': 'time taken to execute bootstrap script',
-                'Startup': 'time between us receiving startup event and leaving event listener during browser startup',
-                'Install': 'time between us receiving startup event and leaving event listener during user-initiated install',
-                'Restart': 'time between us receiving startup event and leaving event listener after user-initiated enable',
-                'StartRequest': 'time spent in the most recent call to the stream converter\'s onStartRequest function',
-                'DataAvailable': 'time spent in the most recent call to the stream converter\'s onDataAvailable function',
-                'ParseJSON': 'time spent parsing the received JSON string into an object',
-                'FormatJSON': 'time spent tokenising the parsed object and generating a string of HTML',
-                'StopRequest': 'time spent in the most recent call to the stream converter\'s onStopRequest function'
-            };
-            for(let measure in TS) {
-                if(TS[measure].length>1) {
-                    log.info(measure + ' Performance: ' + (TS[measure][1]-TS[measure][0]) + 'ms' + (measure in desc ? ' (' + desc[measure] + ')' : ''));
-                }
-            }
-        }
-    });
-
     let prefUtils = require(PLATFORM + '/prefUtils');
-    prefUtils.registerConversions(listenPref);
-    prefUtils.registerExtMap(listenPref);
-    // maybe we can add a lower q-value in the future, track https://issues.apache.org/jira/browse/COUCHDB-234
-    prefUtils.registerAcceptHeader(listenPref, 'acceptHeader.json', 'application/json');
 
+    TS['RegisterConversions'] = [Date.now()];
+    prefUtils.registerConversions(listenPref);
+    TS['RegisterConversions'].push(Date.now());
+
+    TS['RegisterExtMap'] = [Date.now()];
+    prefUtils.registerExtMap(listenPref);
+    TS['RegisterExtMap'].push(Date.now());
+
+    // maybe we can add a lower q-value in the future, track https://issues.apache.org/jira/browse/COUCHDB-234
+    TS['RegisterAcceptHeader'] = [Date.now()];
+    prefUtils.registerAcceptHeader(listenPref, 'acceptHeader.json', 'application/json');
+    TS['RegisterAcceptHeader'].push(Date.now());
+
+    TS['InitResource'] = [Date.now()];
     (function setResourceAlias(alias, target) { // set up resource:// URLs
         let proto = Services.io.getProtocolHandler('resource').QueryInterface(Ci.nsIResProtocolHandler);
         proto.setSubstitution(alias, target);
@@ -94,6 +83,45 @@ function startup() {
             proto.setSubstitution(alias, null);
         });
     })(ADDON_LNAME, getResourceURI('resources/')); // trailing slash required inside XPI
+    TS['InitResource'].push(Date.now());
+
+    TS['PrepareAsyncLoad'] = [Date.now()];
+    let timer = Cc["@mozilla.org/timer;1"].createInstance(Ci.nsITimer);
+    timer.init({ // async load
+        observe: function() {
+            listenPref('debug', function(branch, pref) {
+                let debug = branch.get(pref, 'boolean');
+                let log = require(PLATFORM + '/log');
+                log.setDebug(debug);
+                if(debug) {
+                    let desc = {
+                        'Bootstrap': 'time taken to execute bootstrap script',
+                        'Startup': 'time between us receiving startup event and leaving event listener during browser startup',
+                        'Install': 'time between us receiving startup event and leaving event listener during user-initiated install',
+                        'Restart': 'time between us receiving startup event and leaving event listener after user-initiated enable',
+                        'StartRequest': 'time spent in the most recent call to the stream converter\'s onStartRequest function',
+                        'DataAvailable': 'time spent in the most recent call to the stream converter\'s onDataAvailable function',
+                        'ParseJSON': 'time spent parsing the received JSON string into an object',
+                        'FormatJSON': 'time spent tokenising the parsed object and generating a string of HTML',
+                        'StopRequest': 'time spent in the most recent call to the stream converter\'s onStopRequest function',
+                        'SetDefaultPrefs': 'time spent initialising default preferences',
+                        'RegisterConversions': 'time taken to register stream converters',
+                        'RegisterExtMap': 'time taken to register file extension to type mappings',
+                        'RegisterAcceptHeader': 'time taken to set up Accept header',
+                        'InitResource': 'time taken to register resource:// URLs',
+                        'PrepareAsyncLoad': 'time spent initialising nsiTimer to defer loading non-essentials'
+                    };
+                    for(let measure in TS) {
+                        if(TS[measure].length>1) {
+                            log.info(measure + ' Performance: ' + (TS[measure][1]-TS[measure][0]) + 'ms' + (measure in desc ? ' (' + desc[measure] + ')' : ''));
+                        }
+                    }
+                }
+            });
+            timer = null; // "Users of instances of nsITimer should keep a reference to the timer until it is no longer needed in order to assure the timer is fired."
+        }
+    }, 500, timer.TYPE_ONE_SHOT);
+    TS['PrepareAsyncLoad'].push(Date.now());
 }
 
 function uninstall() {
