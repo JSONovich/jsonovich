b5990bfb785b4c21af58827fcdabfe16eb4a8f1d
W.Elwood08@gmail.com <W.Elwood08@gmail.com@4a0d538c-d5f5-11dd-a297-4fb579711a53>
2011-08-25 07:26:04 +0000

    Fix possible race conditions at startup.
    Avoid asynchronous callback that's only used to obtain a getResourceURI function.
    Registering the stream converter at the very least should happen synchronously at startup.

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -29,62 +29,70 @@ Components.utils['import']("resource://gre/modules/AddonManager.jsm");
 
     global.startup = function startup(data, reason) {
         let measure = reason == APP_STARTUP ? 'Startup' : (reason == ADDON_INSTALL ? 'Install' : 'Restart');
-        AddonManager.getAddonByID(data.id, function(addon) {
-            TS[measure] = [Date.now()];
+        TS[measure] = [Date.now()];
 
-            function getResourceURI(path) {
-                return addon.getResourceURI(path);
-            }
-
-            function getResourceURISpec(path) {
-                return getResourceURI(path).spec;
-            }
-
-            electrolyte = {
-                ADDON_NAME: ADDON_NAME,
-                ADDON_LNAME: ADDON_LNAME,
-                ADDON_DOMAIN: ADDON_DOMAIN,
-                IN_CHROME: true,
-                IN_CONTENT: false,
-                Cc: Components.classes,
-                Ci: Components.interfaces,
-                Cm: Components.manager,
-                Cr: Components.results,
-                Cu: Components.utils,
-                getResourceURI: getResourceURI,
-                getResourceURISpec: getResourceURISpec
-            };
-            messages = {
-                manager: Components.classes["@mozilla.org/globalmessagemanager;1"].getService(Components.interfaces.nsIChromeFrameMessageManager),
-                listener: function bootstrapSyncListener(msg) {
-                    switch(msg.name) {
-                        case ADDON_LNAME + ':getResourceURISpec':
-                            return {
-                                spec: getResourceURI(msg.json.path).spec
-                            };
-                    }
+        function getResourceURI(aPath) { // @see http://mxr.mozilla.org/mozilla-central/source/toolkit/mozapps/extensions/XPIProvider.jsm
+            let bundle = data.installPath.clone();
+            if(aPath) {
+                if(bundle.isDirectory()) {
+                    aPath.split("/").forEach(function(aPart) {
+                        bundle.append(aPart);
+                    });
+                } else {
+                    return Services.io.newURI("jar:" + bundle.spec + "!/" + aPath, null, null);
                 }
-            };
-
-            // unloadable global frame scripts OR no choice (Fennec)
-            if('removeDelayedFrameScript' in messages.manager || Services.appinfo.ID == '{a23983c0-fd0e-11dc-95ff-0800200c9a66}') { // TODO: check if adding support for more platforms
-                messages.manager.addMessageListener(ADDON_LNAME + ':getResourceURISpec', messages.listener);
-                messages.manager.loadFrameScript(getResourceURISpec('modules/content/e10sbootstrap.js'), true);
-                electrolyte.messageManager = messages.manager;
-                // https://bugzilla.mozilla.org/show_bug.cgi?id=681206 cannot be avoided in Fennec
-                // TODO: manually add frame script to each existing and future tab's message manager instead of using global message manager
-                // ref: http://adblockplus.org/jsdoc/adblockplus/symbols/src/modules_AppIntegrationFennec.jsm.html
-            } else { // avoid IPC system, performance of not sending messages is obviously better without multi-process
-                electrolyte.IN_CONTENT = true;
-                messages = null;
             }
+            return Services.io.newFileURI(bundle);
+        }
 
-            Services.scriptloader.loadSubScript(getResourceURISpec('modules/electrolyte.js'), electrolyte);
-            if(electrolyte.startup) {
-                electrolyte.startup();
+        function getResourceURISpec(path) {
+            return getResourceURI(path).spec;
+        }
+
+        electrolyte = {
+            ADDON_NAME: ADDON_NAME,
+            ADDON_LNAME: ADDON_LNAME,
+            ADDON_DOMAIN: ADDON_DOMAIN,
+            IN_CHROME: true,
+            IN_CONTENT: false,
+            Cc: Components.classes,
+            Ci: Components.interfaces,
+            Cm: Components.manager,
+            Cr: Components.results,
+            Cu: Components.utils,
+            getResourceURI: getResourceURI,
+            getResourceURISpec: getResourceURISpec
+        };
+        messages = {
+            manager: Components.classes["@mozilla.org/globalmessagemanager;1"].getService(Components.interfaces.nsIChromeFrameMessageManager),
+            listener: function bootstrapSyncListener(msg) {
+                switch(msg.name) {
+                    case ADDON_LNAME + ':getResourceURISpec':
+                        return {
+                            spec: getResourceURI(msg.json.path).spec
+                        };
+                }
             }
-            TS[measure].push(Date.now());
-        });
+        };
+
+        // unloadable global frame scripts OR no choice (Fennec)
+        if('removeDelayedFrameScript' in messages.manager || Services.appinfo.ID == '{a23983c0-fd0e-11dc-95ff-0800200c9a66}') { // TODO: check if adding support for more platforms
+            messages.manager.addMessageListener(ADDON_LNAME + ':getResourceURISpec', messages.listener);
+            messages.manager.loadFrameScript(getResourceURISpec('modules/content/e10sbootstrap.js'), true);
+            electrolyte.messageManager = messages.manager;
+        // https://bugzilla.mozilla.org/show_bug.cgi?id=681206 cannot be avoided in Fennec
+        // TODO: manually add frame script to each existing and future tab's message manager instead of using global message manager
+        // ref: http://adblockplus.org/jsdoc/adblockplus/symbols/src/modules_AppIntegrationFennec.jsm.html
+        } else { // avoid IPC system, performance of not sending messages is obviously better without multi-process
+            electrolyte.IN_CONTENT = true;
+            messages = null;
+        }
+
+        Services.scriptloader.loadSubScript(getResourceURISpec('modules/electrolyte.js'), electrolyte);
+        if(electrolyte.startup) {
+            electrolyte.startup();
+        }
+        TS[measure].push(Date.now());
     };
 
     global.shutdown = function shutdown(data, reason) {
