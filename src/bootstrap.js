99713bebc40ca5057d7274f37a5e10dc92d47ed6
W.Elwood08@gmail.com <W.Elwood08@gmail.com@4a0d538c-d5f5-11dd-a297-4fb579711a53>
2011-08-25 12:16:18 +0000

    Remove unused import.

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -16,7 +16,6 @@ var TS = {
 
 Components.utils['import']("resource://gre/modules/XPCOMUtils.jsm");
 Components.utils['import']("resource://gre/modules/Services.jsm");
-Components.utils['import']("resource://gre/modules/AddonManager.jsm");
 
 (function(global) {
     'use strict';
