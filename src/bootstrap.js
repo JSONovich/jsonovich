952e2e8e1e2047c8dc2b3cf2424ec5aa68e09e60
W.Elwood08@gmail.com <W.Elwood08@gmail.com@4a0d538c-d5f5-11dd-a297-4fb579711a53>
2011-08-27 22:38:15 +0000

    Hotfix for being completely broken when installed as a packed XPI.
    Naturally I only catch this after tagging and uploading to AMO, sighface.

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -30,17 +30,21 @@ Components.utils['import']("resource://gre/modules/Services.jsm");
         TS[measure] = [Date.now()];
 
         function getResourceURI(aPath) { // @see http://mxr.mozilla.org/mozilla-central/source/toolkit/mozapps/extensions/XPIProvider.jsm
-            let bundle = data.installPath.clone();
+            let doJar = false, bundle = data.installPath.clone();
             if(aPath) {
                 if(bundle.isDirectory()) {
                     aPath.split("/").forEach(function(aPart) {
                         bundle.append(aPart);
                     });
                 } else {
-                    return Services.io.newURI("jar:" + bundle.spec + "!/" + aPath, null, null);
+                    doJar = true;
                 }
             }
-            return Services.io.newFileURI(bundle);
+            bundle = Services.io.newFileURI(bundle);
+            if(doJar) {
+                return Services.io.newURI("jar:" + bundle.spec + "!/" + aPath, null, null);
+            }
+            return bundle;
         }
 
         function getResourceURISpec(path) {
