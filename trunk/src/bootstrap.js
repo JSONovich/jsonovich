4c4e5ebeb42ecbda4edffd27794cc577cdb253ad
W.Elwood08@gmail.com <W.Elwood08@gmail.com@4a0d538c-d5f5-11dd-a297-4fb579711a53>
2012-05-01 19:00:37 +0000

    Merge separate chrome and content bootstrap scripts into a single file that handles both situations and shares code where possible.

--- trunk/src/bootstrap.js
+++ trunk/src/bootstrap.js
@@ -6,10 +6,8 @@
  *
  * Changelog:
  * [2011-05] - Created FF4 restartless bootstrap for JSONovich extension
+ * [2012-05] - Merged E10S content script bootstrap into here to share code
  */
-
-Components.utils['import']("resource://gre/modules/XPCOMUtils.jsm");
-Components.utils['import']("resource://gre/modules/Services.jsm");
 
 (function(global) {
     'use strict';
@@ -21,17 +19,25 @@ Components.utils['import']("resource://gre/modules/Services.jsm");
     ADDON_LNAME = 'jsonovich',
     ADDON_DOMAIN = 'lackoftalent.org',
     electrolyte = null,
-    messages = null;
+    ipcServices = {},
+    PRIVILEDGED = typeof global.sendSyncMessage === 'undefined',
+    Services = null;
 
-    global.startup = function startup(data, reason) {
-        let measure = reason == APP_STARTUP ? 'Startup' : (reason == ADDON_INSTALL ? 'Install' : 'Restart');
+    function scopedImport(path, scope) {
+        scope = scope ? scope : {};
+        Components.utils['import'](path, scope);
+        return scope;
+    }
+
+    function startup(data, reason) {
+        var measure = typeof reason === 'string' ? reason : (reason === APP_STARTUP ? 'Startup' : (reason === ADDON_INSTALL ? 'Install' : 'Restart'));
         TS[measure] = [Date.now()];
 
         function getResourceURI(aPath) { // @see http://mxr.mozilla.org/mozilla-central/source/toolkit/mozapps/extensions/XPIProvider.jsm
-            let doJar = false, bundle = data.installPath.clone();
+            var doJar = false, bundle = data.installPath.clone();
             if(aPath) {
                 if(bundle.isDirectory()) {
-                    aPath.split("/").forEach(function(aPart) {
+                    aPath.split('/').forEach(function(aPart) {
                         bundle.append(aPart);
                     });
                 } else {
@@ -40,7 +46,7 @@ Components.utils['import']("resource://gre/modules/Services.jsm");
             }
             bundle = Services.io.newFileURI(bundle);
             if(doJar) {
-                return Services.io.newURI("jar:" + bundle.spec + "!/" + aPath, null, null);
+                return Services.io.newURI('jar:' + bundle.spec + '!/' + aPath, null, null);
             }
             return bundle;
         }
@@ -54,72 +60,121 @@ Components.utils['import']("resource://gre/modules/Services.jsm");
             ADDON_NAME: ADDON_NAME,
             ADDON_LNAME: ADDON_LNAME,
             ADDON_DOMAIN: ADDON_DOMAIN,
-            IN_CHROME: true,
-            IN_CONTENT: false,
+            IN_CHROME: PRIVILEDGED,
+            IN_CONTENT: !PRIVILEDGED,
             Cc: Components.classes,
             Ci: Components.interfaces,
             Cm: Components.manager,
             Cr: Components.results,
             Cu: Components.utils,
             getResourceURI: getResourceURI,
-            getResourceURISpec: getResourceURISpec
+            getResourceURISpec: getResourceURISpec,
+            Services: Services
         };
-        messages = {
-            manager: Components.classes["@mozilla.org/globalmessagemanager;1"].getService(Components.interfaces.nsIChromeFrameMessageManager),
-            listener: function bootstrapSyncListener(msg) {
+        scopedImport('resource://gre/modules/XPCOMUtils.jsm', electrolyte);
+
+        if(PRIVILEDGED) { /*!! CHROME !!*/
+            ipcServices.messageManager = Components.classes['@mozilla.org/globalmessagemanager;1'].getService(Components.interfaces.nsIChromeFrameMessageManager);
+            ipcServices.messageListener = function bootstrapSyncListener(msg) {
                 switch(msg.name) {
                     case ADDON_LNAME + ':getStartupConstants':
                         return {
                             installPath: data.installPath.path
                         };
                 }
+            };
+            // unloadable global frame scripts OR no choice (Fennec)
+            if('removeDelayedFrameScript' in ipcServices.messageManager || Services.appinfo.ID == '{a23983c0-fd0e-11dc-95ff-0800200c9a66}') { // TODO: check if adding support for more platforms
+                ipcServices.messageManager.addMessageListener(ADDON_LNAME + ':getStartupConstants', ipcServices.messageListener);
+                ipcServices.messageManager.loadFrameScript(getResourceURISpec('bootstrap.js'), true);
+                electrolyte.messageManager = ipcServices.messageManager;
+            // https://bugzilla.mozilla.org/show_bug.cgi?id=681206 cannot be avoided in Fennec
+            // TODO: manually add frame script to each existing and future tab's message manager instead of using global message manager
+            // ref: http://adblockplus.org/jsdoc/adblockplus/symbols/src/modules_AppIntegrationFennec.jsm.html
+            } else {
+                electrolyte.IN_CONTENT = true;
+                ipcServices = {};
             }
-        };
-
-        // unloadable global frame scripts OR no choice (Fennec)
-        if('removeDelayedFrameScript' in messages.manager || Services.appinfo.ID == '{a23983c0-fd0e-11dc-95ff-0800200c9a66}') { // TODO: check if adding support for more platforms
-            messages.manager.addMessageListener(ADDON_LNAME + ':getStartupConstants', messages.listener);
-            messages.manager.loadFrameScript(getResourceURISpec('modules/content/e10sbootstrap.js'), true);
-            electrolyte.messageManager = messages.manager;
-        // https://bugzilla.mozilla.org/show_bug.cgi?id=681206 cannot be avoided in Fennec
-        // TODO: manually add frame script to each existing and future tab's message manager instead of using global message manager
-        // ref: http://adblockplus.org/jsdoc/adblockplus/symbols/src/modules_AppIntegrationFennec.jsm.html
-        } else { // avoid IPC system, performance in a single process is obviously better if not sending messages
-            electrolyte.IN_CONTENT = true;
-            messages = null;
+        } else { /*-- content --*/
+            ipcServices.messageManager = global;
+            ipcServices.once = {
+                path: getResourceURISpec('modules/content/OncePerProcess.jsm')
+            };
+            ipcServices.messageManager.addMessageListener(ADDON_LNAME + ':shutdown', shutdown);
+            Components.utils['import'](ipcServices.once.path, ipcServices.once);
         }
 
         Services.scriptloader.loadSubScript(getResourceURISpec('modules/electrolyte.js'), electrolyte);
         if(electrolyte.startup) {
-            electrolyte.startup();
+            electrolyte.startup(ipcServices.once);
         }
         TS[measure].push(Date.now());
     };
 
-    global.shutdown = function shutdown(data, reason) {
+    function shutdown(data, reason) {
         if(electrolyte != null) {
-            if(reason == ADDON_UNINSTALL) {
-                if(electrolyte.uninstall) {
-                    electrolyte.uninstall();
-                }
-            }
-            if(reason != APP_SHUTDOWN) {
-                if(messages) {
-                    if('removeDelayedFrameScript' in messages.manager) {
-                        messages.manager.removeDelayedFrameScript(electrolyte.getResourceURISpec('modules/content/e10sbootstrap.js'));
+            if(PRIVILEDGED) { /*!! CHROME !!*/
+                if(reason == ADDON_UNINSTALL) {
+                    if(electrolyte.uninstall) {
+                        electrolyte.uninstall();
                     }
-                    messages.manager.sendAsyncMessage(ADDON_LNAME + ':shutdown', {});
-                    messages.manager.removeMessageListener(ADDON_LNAME + ':getStartupConstants', messages.listener);
                 }
+                if(reason != APP_SHUTDOWN) {
+                    if(ipcServices.messageManager) {
+                        if('removeDelayedFrameScript' in ipcServices.messageManager) {
+                            ipcServices.messageManager.removeDelayedFrameScript(electrolyte.getResourceURISpec('modules/content/e10sbootstrap.js'));
+                        }
+                        ipcServices.messageManager.sendAsyncMessage(ADDON_LNAME + ':shutdown', {});
+                        ipcServices.messageManager.removeMessageListener(ADDON_LNAME + ':getStartupConstants', ipcServices.messageListener);
+                    }
+                    if(electrolyte.shutdown) {
+                        electrolyte.shutdown();
+                    }
+                    ipcServices = null;
+                }
+            } else { /*-- content --*/
+                ipcServices.messageManager.removeMessageListener(ADDON_LNAME + ':shutdown', shutdown);
                 if(electrolyte.shutdown) {
                     electrolyte.shutdown();
                 }
-                messages = null;
+                ipcServices.once.resetOncePerProcess();
+                if(typeof Components.utils['unload'] == 'function') {
+                    Components.utils['unload'](ipcServices.once.path);
+                }
+                ipcServices = {};
             }
+            electrolyte = null;
         }
-        electrolyte = null;
     };
 
-    global.install = global.uninstall = function() {};
+    if(PRIVILEDGED) { /*!! CHROME !!*/
+        Services = scopedImport('resource://gre/modules/Services.jsm').Services;
+        global.startup = startup;
+        global.shutdown = shutdown;
+        global.install = global.uninstall = function() {};
+    } else { /*-- content --*/
+        global.addEventListener('DOMContentLoaded', function load(event) {
+            function content_ensureStartupConstants() {
+                var reply = global.sendSyncMessage(ADDON_LNAME + ':getStartupConstants', {});
+                if(reply.length) {
+                    var data = {
+                        installPath: Components.classes['@mozilla.org/file/local;1'].createInstance(Components.interfaces.nsILocalFile)
+                    };
+                    data.installPath.initWithPath(reply[0].installPath);
+                    startup(data, 'Startup');
+                } else {
+                    content_scheduleSyncMessage();
+                }
+            }
+            function content_scheduleSyncMessage() {
+                Services.tm.mainThread.dispatch({
+                    run: content_ensureStartupConstants
+                }, Components.interfaces.nsIThread.DISPATCH_NORMAL);
+            }
+            global.removeEventListener('DOMContentLoaded', load, false);
+            Services = scopedImport('resource://gre/modules/Services.jsm').Services;
+            content_scheduleSyncMessage();
+        }, false);
+    }
     TS['Bootstrap'].push(Date.now());
 })(this);
