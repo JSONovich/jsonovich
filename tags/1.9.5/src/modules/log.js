69adb58931afc98290fe3858cad04d272798637c
W.Elwood08@gmail.com <W.Elwood08@gmail.com@4a0d538c-d5f5-11dd-a297-4fb579711a53>
2012-06-28 22:05:34 +0000

    Hotfix for serious memory leak and Gecko 15+ compatibility

---
+++ tags/1.9.5/src/modules/log.js
@@ -0,0 +1,36 @@
+/**
+ * @license MPL 1.1/GPL 2.0/LGPL 2.1, see license.txt
+ * @author William Elwood <we9@kent.ac.uk>
+ * @copyright 2011 JSONovich Team. All Rights Reserved.
+ * @copyright Portions (C) 2010 the Mozilla Foundation.
+ * @description This file contains utility functions for logging to the error console.
+ *
+ * Changelog:
+ * [2011-05] - Created log module
+ * [2011-06] - Don't clobber Error objects
+ */
+
+'use strict';
+
+let debug = false,
+prefixError = '[' + ADDON_NAME + ' Error] ',
+prefixInfo = '[' + ADDON_NAME + ' Info] ',
+prefixDebug = '[' + ADDON_NAME + ' Debug] ';
+
+exports.error = function logError(err) {
+    Cu.reportError(err instanceof Error ? err : prefixError + err);
+}
+
+exports.info = function logInfo(msg) {
+    Services.console.logStringMessage(prefixInfo + msg);
+}
+
+exports.debug = function logDebug(msg) {
+    if(debug) {
+        Services.console.logStringMessage(prefixDebug + msg);
+    }
+}
+
+exports.setDebug = function setDebug(enable) {
+    debug = enable;
+}
