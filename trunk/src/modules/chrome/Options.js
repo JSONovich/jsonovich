6813e139cb5ae82552c4ae4f9be8ae5017f2a0bd
W.Elwood08@gmail.com <W.Elwood08@gmail.com@4a0d538c-d5f5-11dd-a297-4fb579711a53>
2012-07-06 00:12:57 +0000

    Minor function change in inline options fix.

--- trunk/src/modules/chrome/Options.js
+++ trunk/src/modules/chrome/Options.js
@@ -25,7 +25,7 @@ observer = {
         if(aTopic != 'addon-options-displayed' || aData != ADDON_LNAME + '@' + ADDON_DOMAIN) {
             return;
         }
-        var container = aSubject.querySelector('#detail-rows'); // normal add-ons manager
+        var container = aSubject.getElementById('detail-rows'); // normal add-ons manager
         if(!container) { // fennec XBL add-ons manager
             container = aSubject.getAnonymousElementByAttribute(aSubject.querySelector('[addonID="'+aData+'"]'), 'anonid', 'options-box');
         }
