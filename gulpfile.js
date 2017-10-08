30066105f5be8f1c52eafabb442b773f179adf52
William Elwood <we9@kent.ac.uk>
2017-10-08 16:32:29 +0100

    Version bump build task and npm hooks
    Use package.json as the source of truth for the version number and a Gulp task to update manifest.json automatically. When it's time for a new version, `npm version <x>` will bump, commit, tag and push.

--- gulpfile.js
+++ gulpfile.js
@@ -9,18 +9,45 @@
 
 const gulp = require('gulp');
 const plugin = {
+    get jsonModify() {
+      delete this.jsonModify;
+      return this.jsonModify = require('gulp-json-modify');
+    },
     get zip() {
       delete this.zip;
       return this.zip = require('gulp-vinyl-zip');
     },
 };
+const data = {
+    get package() {
+        delete this.package;
+        return this.package = require('./package.json');
+    }
+};
 
+/**
+ * Update version numbers in the source files using package.json as the source of truth.
+ * Intended to be called by npm after it has incremented the package.json version.
+ */
+function version() {
+    return gulp.src(['src/manifest.json'], {base: '.'})
+        .pipe(plugin.jsonModify({
+            key: 'version',
+            value: data.package.version.replace(/-([^\.0-9]+)\./, '$1').replace(/(\d)-(\d)/, '$1pre$2')
+        }))
+        .pipe(gulp.dest('.'));
+}
+
+/**
+ * Build the Firefox .xpi from the source files.
+ */
 function xpi() {
     return gulp.src('src/**/*', { nodir: true })
         .pipe(plugin.zip.zip('jsonovich.xpi'))
         .pipe(gulp.dest('build'));
 }
 
+gulp.task('version', version);
 gulp.task('xpi', xpi);
 gulp.task('build', ['xpi']);
 gulp.task('default', ['build']);
